ffa1da6406408a242f816f7eb199fda40f4ab9f7
Gregory Boddin <gregory@siwhine.net>
2016-04-28 20:52:48 +0200

    Fixed log file path

--- resources/codedeploy/scripts/install.sh
+++ resources/codedeploy/scripts/install.sh
@@ -6,5 +6,5 @@ do
   sleep 2
 done
 
-bin/phing -propertyfile /usr/local/etc/subsite/subsite.ini install >> /var/log/subsite/install.log 2>&1
+bin/phing -propertyfile /usr/local/etc/subsite/subsite.ini install >> /var/log/subsite-install.log 2>&1
 chown -R www-data:www-data /srv/project
