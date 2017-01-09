90fc704cc554a8dfe808e180b6dfd52e3ed65369
GERVAIS Sebastien <sebastien.gervais@ext.ec.europa.eu>
2017-01-09 16:30:18 +0100

    Behat: Move fillCaptcha to DrupalContext class
    $this->user is not available on RawDrupalContext, fillCaptcha context moved to DrupalContext
    NEXTEUROPA-13253

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -169,22 +169,4 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
     }
   }
 
-  /**
-   * Fill a captcha the captcha on the page.
-   *
-   * @Then (I )fill the captcha
-   */
-  public function fillCaptcha() {
-    // Get the last captcha of this user.
-    $query = 'SELECT MAX(csid), solution FROM {captcha_sessions} WHERE uid=:uid';
-    $result = db_query($query, [':uid' => $this->user->uid]);
-    $record = $result->fetchAssoc();
-
-    if (!isset($record['solution'])) {
-      throw new ExpectationException('The captcha solution could not be found', $this->getSession());
-    }
-    // Put the solution in the field.
-    $this->getSession()->getPage()->fillField('edit-captcha-response', $record['solution']);
-  }
-
 }
