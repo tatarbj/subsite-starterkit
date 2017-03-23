35645e03af45084a3c36026be748268d4ba6272f
Alex Verbruggen <verbruggenalex@gmail.com>
2017-03-23 18:35:41 +0100

    PHPCS

--- src/Phing/ComposerTask.php
+++ src/Phing/ComposerTask.php
@@ -7,31 +7,31 @@ require_once "phing/Task.php";
 /**
  * Extension on the phing composer task that checks for paths.
  */
-class ComposerFallbackTask extends \ComposerTask
-{
-    /**
-     * Executes the Composer task.
-     */
-    public function main()
-    {
-        $composerFile = new SplFileInfo($this->getComposer());
-        if (false === $composerFile->isFile()) {
-            exec('which composer', $composerLocation, $return);
-            if ($return === 0) {
-              $this->setComposer($composerLocation[0]);
-            }
-            else {
-              throw new BuildException(sprintf('Composer binary not found, path is "%s"', $composerFile));
-            }
-        }
+class ComposerFallbackTask extends \ComposerTask {
 
-        $commandLine = $this->prepareCommandLine();
-        $this->log("executing " . $commandLine);
-
-        passthru($commandLine, $return);
-
-        if ($return > 0) {
-            throw new BuildException("Composer execution failed");
-        }
+  /**
+   * Executes the Composer task.
+   */
+  public function main() {
+    $composerFile = new SplFileInfo($this->getComposer());
+    if (FALSE === $composerFile->isFile()) {
+      exec('which composer', $composerLocation, $return);
+      if ($return === 0) {
+        $this->setComposer($composerLocation[0]);
+      }
+      else {
+        throw new BuildException(sprintf('Composer binary not found, path is "%s"', $composerFile));
+      }
     }
+
+    $commandLine = $this->prepareCommandLine();
+    $this->log("executing " . $commandLine);
+
+    passthru($commandLine, $return);
+
+    if ($return > 0) {
+      throw new BuildException("Composer execution failed");
+    }
+  }
+
 }
