379d6969f87142012e7e486f724379239a5de68f
Alex Verbruggen <verbruggenalex@gmail.com>
2017-05-04 09:37:10 +0200

    Fix CS

--- src/Drupal/generate-directories.php
+++ src/Drupal/generate-directories.php
@@ -25,6 +25,7 @@ foreach ($directories as $directory) {
     // Let mkdir() recursively create directories and use the default directory
     // permissions.
     if (@drupal_mkdir($directory, NULL, TRUE)) {
+      // @codingStandardsIgnoreLine
       @chmod($directory, 0775)
     }
   }
