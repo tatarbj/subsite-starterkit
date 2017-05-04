b3d7efced9bc45d305e76d6915c415df890b390a
Alex Verbruggen <verbruggenalex@gmail.com>
2017-05-04 06:58:58 +0200

    Improve dir creation

--- src/Drupal/generate-directories.php
+++ src/Drupal/generate-directories.php
@@ -7,14 +7,16 @@
 
 // Directories to create.
 $directories = array(
-  variable_get('file_temporary_path', file_directory_temp()),
+  variable_get('file_temporary_path', conf_path() . '/tmp'),
   variable_get('file_private_path', conf_path() . '/files/private_files'),
-  variable_get('file_public_path', conf_path() . '/files/css_injector'),
-  variable_get('file_public_path', conf_path() . '/files/js_injector'),
+  variable_get('file_public_path', conf_path() . '/files') . '/css_injector',
+  variable_get('file_public_path', conf_path() . '/files') . '/js_injector',
   variable_get('file_public_path', conf_path() . '/files'),
 );
 
 foreach ($directories as $directory) {
-  file_prepare_directory($directory, FILE_CREATE_DIRECTORY);
-  drupal_chmod($directory);
+  if ($directory) {
+    file_prepare_directory($directory, FILE_CREATE_DIRECTORY);
+    drupal_chmod($directory);
+  }
 }
