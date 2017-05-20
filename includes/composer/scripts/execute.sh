3e0b8d43f434a01705495758bf6e7a7ea89457b3
Alex Verbruggen <verbruggenalex@gmail.com>
2017-05-20 15:19:35 +0200

    Colorize

--- includes/composer/scripts/execute.sh
+++ includes/composer/scripts/execute.sh
@@ -3,7 +3,7 @@
 # Post install or update.
 PHING=$(pwd)/bin/phing
 if [ -f $PHING ] && [ -x $PHING ] ; then
-  $PHING composer-$1-cmd -Dproject.basedir=$PROJECT -find
+  $PHING composer-$1-cmd -Dproject.basedir=$PROJECT -find --ansi
 else
   echo "Phing unavailable"
 fi
