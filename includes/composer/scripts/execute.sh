ba343b9950411e13e7844033cd683f701e8b3b4b
Alex Verbruggen <verbruggenalex@gmail.com>
2017-05-20 15:06:47 +0200

    Fix var

--- includes/composer/scripts/execute.sh
+++ includes/composer/scripts/execute.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Post install or update.
-PHING = $(pwd)/bin/phing
-if [ -f $PHING ]; then
+PHING=$(pwd)/bin/phing
+if [ -f $PHING ] && [ -x $PHING ] ; then
   $PHING $1 -Dproject.basedir=$PROJECT -find
 else
   echo "Phing unavailable"
