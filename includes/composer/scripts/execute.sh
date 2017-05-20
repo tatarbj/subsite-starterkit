9686b61cfcefcbab3f0c51e2f7a283b44c2544d1
Alex Verbruggen <verbruggenalex@gmail.com>
2017-05-20 15:21:09 +0200

    Try with color

--- includes/composer/scripts/execute.sh
+++ includes/composer/scripts/execute.sh
@@ -3,7 +3,7 @@
 # Post install or update.
 PHING=$(pwd)/bin/phing
 if [ -f $PHING ] && [ -x $PHING ] ; then
-  $PHING composer-$1-cmd -Dproject.basedir=$PROJECT -find --ansi
+  $PHING composer-$1-cmd --ansi -Dproject.basedir=$PROJECT -find
 else
   echo "Phing unavailable"
 fi
