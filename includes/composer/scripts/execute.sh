e07fb55213bb5abdbce19ce2297011e3165bee7e
Alex Verbruggen <verbruggenalex@gmail.com>
2017-05-20 15:13:12 +0200

    Cleanup names

--- includes/composer/scripts/execute.sh
+++ includes/composer/scripts/execute.sh
@@ -3,7 +3,7 @@
 # Post install or update.
 PHING=$(pwd)/bin/phing
 if [ -f $PHING ] && [ -x $PHING ] ; then
-  $PHING $1 -Dproject.basedir=$PROJECT -find
+  $PHING composer-$1-cmd -Dproject.basedir=$PROJECT -find
 else
   echo "Phing unavailable"
 fi
