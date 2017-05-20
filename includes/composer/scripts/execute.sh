ba45cf601fbfb0eeb8ed24daef598a3f87f2c57e
Alex Verbruggen <verbruggenalex@gmail.com>
2017-05-20 15:24:30 +0200

    colorlogger

--- includes/composer/scripts/execute.sh
+++ includes/composer/scripts/execute.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # Post install or update.
+
 PHING=$(pwd)/bin/phing
 if [ -f $PHING ] && [ -x $PHING ] ; then
-  $PHING composer-$1-cmd --ansi -Dproject.basedir=$PROJECT -find
+  $PHING composer-$1-cmd -Dproject.basedir=$PROJECT -logger phing.listener.AnsiColorLogger -find
 else
   echo "Phing unavailable"
 fi
