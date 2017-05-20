63bdbeea1079599be8df4552fc94d4c63f0263ad
Alex Verbruggen <verbruggenalex@gmail.com>
2017-05-20 15:04:01 +0200

    Phing composer execs

--- includes/composer/scripts/execute.sh
+++ includes/composer/scripts/execute.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
-# Symlink starterkit bin.
-$(pwd)/bin/phing link-ssk-bin -Dproject.basedir=$PROJECT -find
-
-# Execute all scripts in the folder that matches the argument.
-DIRECTORY="$(dirname $0)"
-HOOK="$1"
-if [ -d "$DIRECTORY/$HOOK" ] ; then
-  for SCRIPT in $DIRECTORY/$HOOK/*
-  do
-    if [ -f $SCRIPT ] && [ -x $SCRIPT ] ; then
-      $SCRIPT
-    fi
-  done
+# Post install or update.
+PHING = $(pwd)/bin/phing
+if [ -f $PHING ]; then
+  $PHING $1 -Dproject.basedir=$PROJECT -find
+else
+  echo "Phing unavailable"
 fi
